5082b919001a6da570dd72031a29e33ee5660459
AminDjb <aamine.djebbari@gmail.com>
2019-11-11 21:47:01 +0100

    Test des boutons d'ajout

--- tests/ui/integration/ui/jetpack-list.spec.js
+++ tests/ui/integration/ui/jetpack-list.spec.js
@@ -5,7 +5,22 @@ context('Jetpack list', () => {
         cy.visit('/')
     })
 
-    it('List all Jetpacks', () => {
-        cy.contains('Jetpack Fortnite Wiki\n')
+    it('Test adding button', () => {
+        cy.get('#add-button').click()
+        
+        //test button close
+        cy.wait(1000)
+        cy.contains('Close').click()
+        cy.wait(1000)
+        cy.get('#add-button').click()
+        
+        //test save
+        cy.wait(1000)
+        cy.get('#save').click()
+        cy.wait(1000)
+        cy.scrollTo('top')
+        
+        //cheack results
+        cy.get('#edit-image-a8019ec0-bfdc-4140-9dbz-4927e5ef5c8d').should('be.visible')
     })
 });
