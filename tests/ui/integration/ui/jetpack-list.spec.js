ed96fe68b8686d6f07c1a4344ff0ea11e1fc4854
AminDjb <aamine.djebbari@gmail.com>
2019-11-11 21:54:46 +0100

    Test des reservations

--- tests/ui/integration/ui/jetpack-list.spec.js
+++ tests/ui/integration/ui/jetpack-list.spec.js
@@ -23,4 +23,47 @@ context('Jetpack list', () => {
         //cheack results
         cy.get('#edit-image-a8019ec0-bfdc-4140-9dbz-4927e5ef5c8d').should('be.visible')
     })
+
+    it('Test reservation', () => {
+        //setup date
+        cy.scrollTo('bottom')
+        cy.wait(1000)
+        cy.get('#startDate').type('1998-07-26')
+        cy.wait(1000)
+        cy.get('#endDate').type('1999-07-26')
+        cy.wait(1000)
+
+        //start search
+        cy.contains('Rechercher').click()
+        cy.wait(1000)
+
+        //catch if no error
+        cy.get('#startDate')
+            .should(($input) => {
+                expect($input).to.have.css('background-color', "rgb(255, 255, 255)");
+            });
+        cy.get('#endDate')
+            .should(($input) => {
+                expect($input).to.have.css('background-color', "rgb(255, 255, 255)");
+            });
+        
+        //cheack results
+        cy.get('#book-image-1').should('be.visible')
+        cy.scrollTo('bottom')
+        cy.wait(1000)
+        cy.get('#1').click()
+        cy.wait(1000)
+        
+        //test button Close/Valider
+        cy.get('#close-book').click()
+        cy.wait(1000)
+        cy.get('#1').click()
+        cy.wait(1000)
+        cy.get('#book').click()
+        cy.wait(1000)
+
+        //cheack booking
+        cy.get('#book-image-a8019ec0-bfdc-4140-9dbz-4927e5ef5c8d').should('be.visible')
+
+    })
 });
